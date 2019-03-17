cc73b074036925753fdceebc783bfb41fb454cf0
Aditya Hase <aditya@adityahase.com>
2019-03-17 13:23:48 +0530

    feat: Improve UI to sync changes correctly

--- static/emulator.js
+++ static/emulator.js
@@ -3,22 +3,29 @@ var App = new Vue({
     template: `
         <div class="bg-light">
             <div class="flex">
-            <div class="w-1/2 h-screen">
+            <div class="w-3/4 h-screen">
                 <h3>Registers</h3>
-                <div v-for="(m, i) in states[current].registers">
-                    {{i}} : <input class="border" type="number" v-model.number="states[current].registers[i]" />
-                </div>
-                <div>A: <input class="border" type="number" v-model.number="states[current].registers.A" /></div>
+                <table class="table-fixed text-center">
+                    <thead>
+                        <tr>
+                            <th v-for="(m, i) in states[current].registers">{{i}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td v-for="(m, i) in states[current].registers"><input class="border w-12 text-right" v-model.number="states[current].registers[i]"/></td>
+                        </tr>
+                    </tbody>
+                </table>
                 <h3>Memory</h3>
-                <div v-for="(m, i) in states[current].memory">
-                    M : <input class="border" type="number" v-model.number="states[current].memory[i]" />
+                <div v-for="chunk in [...Array(16).keys()]">
+                    <span v-for="(m, i) in states[current].memory.slice(chunk*16, chunk*16 + 16)">
+                        <input class="border w-12 text-right" v-model.number="states[current].memory[chunk*16+i]" />
+                    </span>
                 </div>
             </div>
-            <div class="w-1/2 bg-grey-lighter h-screen">
-                <div @click="setState(h)" v-for="h in history">
-                {{ h.program }}
-                </div>
-                <textarea class="border" cols="40" rows="10" @keydown.enter="submit"></textarea>
+            <div class="w-1/4 bg-grey-lighter h-screen">
+                <textarea class="border" cols="40" rows="10" @keydown.meta.enter="submit"></textarea>
             </div>
             </div>
         </div>
@@ -27,13 +34,10 @@ var App = new Vue({
         return {
             states: [
                 {
-                    registers: {
-                        A: 0,
-                        B: 0
-                    },
-                    memory: [0, 0, 0, 0],
-                    }
-                ],
+                    registers: {},
+                    memory: [],
+                }
+            ],
             current: 0,
             history: []
         }
@@ -63,21 +67,24 @@ var App = new Vue({
             });
             if (res.ok) {
                 const response = await res.json();
-                const laststate = response.slice(-1)[0];
-                this.registers = laststate.registers;
-                this.memory = laststate.memory;
-                this.history.push({
-                    program,
-                    state: {
-                        registers: laststate.registers,
-                        memory: laststate.memory
-                    }
-                });
+                this.states = response;
+                this.current = this.states.length - 1;
+
+                // const laststate = response.slice(-1)[0];
+                // console.log(laststate);
+                // this.history.push({
+                //     program,
+                //     state: {
+                //         registers: laststate.registers,
+                //         memory: laststate.memory
+                //     }
+                // });
             }
         },
-        setState(h) {
-            this.registers = h.state.registers;
-            this.memory = h.state.memory;
-        }
+        // setState(h) {
+        //     console.log(h);
+        //     this.registers = h.state.registers;
+        //     this.memory = h.state.memory;
+        // }
     }
 })
